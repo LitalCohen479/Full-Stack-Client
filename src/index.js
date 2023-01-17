027eb4522bdd02c52ae6a693212870882e4910ca
Lital Cohen <litalcohen479@gmail.com>
2023-01-17 18:52:47 +0200

    cors

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+const cors= require('cors');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
