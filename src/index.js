8c348eeb5060b7212802406d0f13a2d31755334b
Lital Cohen <litalcohen479@gmail.com>
2023-01-17 19:11:21 +0200

    git

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+const cors= require('cors');
+cors.AccessControlAllowOrigin("https://litalcohenfullstack.herokuapp.com");
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
