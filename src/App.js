95d3f4676c58f4086156c579b21bb4c6fda89207
Lital Cohen <litalcohen479@gmail.com>
2023-01-17 18:14:27 +0200

    hashrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom'
+import {HashRouter as Router, Route, Routes, Link} from 'react-router-dom'
 import Home from './pages/Home'
 import CreatePost from './pages/CreatePost'
 import Post from './pages/Post';
